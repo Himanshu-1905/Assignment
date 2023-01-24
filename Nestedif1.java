3357a6f7ad0f42320f1b8eb9e59c156d6dd57f21
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-01-24 23:21:05 +0530

    Add files via upload

---
+++ Nestedif1.java
@@ -0,0 +1,16 @@
+class Nestedif1{
+public static void main(String[] args){
+
+int num1 = 3;
+int num2 = 4;
+
+if(num1==5){
+if(num2==6){
+System.out.println("wrong statement");
+}
+else{
+System.out.println("Not matched");
+}}
+else{
+System.out.println("1st condition not satisfied");
+}}}
