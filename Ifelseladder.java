93cf6d212fa14a9db03614771abbb3578706885d
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-01-25 12:16:44 +0530

    Add files via upload

---
+++ Ifelseladder.java
@@ -0,0 +1,17 @@
+class Ifelseladder{
+public static void main(String[] args){
+String name = "Himanshu";
+ if(name =="Jatin") {
+System.out.println("Name is Jatin");
+}
+ else if (name =="Harsh") {
+System.out.println("Name is Harsh");
+}
+else if (name =="Sachin") {
+System.out.println("Name is Sachin");
+}
+else {
+System.out.println(name);
+}
+}
+}
