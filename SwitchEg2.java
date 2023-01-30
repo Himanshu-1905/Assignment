75f139c04c383ec3dd815ffd44fe8572f1614568
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-01-30 18:31:07 +0530

    Add files via upload

---
+++ SwitchEg2.java
@@ -0,0 +1,25 @@
+
+
+public class SwitchEg2 {
+	public static void main(String[] args) { //main=ctrl+space
+		String college="DSD";
+		switch (college) {
+		case "DU":
+			System.out.println("Delhi University");
+			break;
+		case "GU":
+			System.out.println("Gurugram University");
+			break;
+		case "MDU":
+			System.out.println("M D U");
+			break;
+		case "DSD":
+			System.out.println("Dronacharty Governmemnt College");
+			break;
+			default:System.out.println("Invalid College");
+			
+		}
+			
+	}
+
+}
