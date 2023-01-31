168915b4130c92d12c3dc2b8243901b89157dd85
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-01-31 15:42:07 +0530

    Add files via upload

---
+++ PyramidPattern3.java
@@ -0,0 +1,29 @@
+
+
+public class PyramidPattern3 {
+	public static void main(String[] args) {
+		for(int i=5; i>=1; i--) // outer loop
+		{
+			if(i%2 !=0) { // odd numbers (1,3,5)
+				for(int j=5; j>=i; j--) {
+		      System.out.print(" * ");
+				}
+			      System.out.println();
+			}
+		}
+		for(int i=2; i<=5; i++)
+		{
+			if(i%2 !=0) {
+				for(int j=5; j>=i; j--) {
+		      System.out.print(" * ");
+				}
+			      System.out.println();
+			}
+		}
+	}
+}
+		
+
+
+
+
