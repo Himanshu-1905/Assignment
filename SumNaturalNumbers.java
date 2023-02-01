6c1194636eaccda1578494bd93983bb9daa2caaa
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-02-01 16:32:40 +0530

    Add files via upload

---
+++ SumNaturalNumbers.java
@@ -0,0 +1,13 @@
+
+
+public class SumNaturalNumbers {
+	public static void main(String[] args) {
+		int num=10, sum=0;
+		for(int i=1; i<=num; ++i)
+		{
+			sum+=i;
+		}
+		System.out.println("Sum of first 10 natural numbers is : " + sum);
+	}
+
+}
