abf179087502a24771f6837613ddf8cb7f8f7107
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-02-17 10:40:35 +0530

    Add files via upload

---
+++ Calculator/MyCalculator.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+class MyCalculator implements AdvancedArithmetic {
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      MyCalculator c = new MyCalculator();
+      System.out.print("Enter a number: ");
+      int n = sc.nextInt();
+      c.divisorSum(n);
+   }
+}
