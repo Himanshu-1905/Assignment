30c363ec0954cf6e5ae0efd3d98f4954ec4c220b
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-02-27 22:54:39 +0530

    Add files via upload

---
+++ AbstractMethod1.java
@@ -0,0 +1,24 @@
+package ControlStatement;
+abstract class BankAccount{
+	int account_number=5456565;
+	int balance= 30000;
+	abstract void deposit();
+	abstract void withdraw();
+}
+class CheckingAccount extends BankAccount{
+	void deposit() {
+		int deposit=1000;
+		System.out.println("Ammount deposit"+" "+(balance+deposit));
+	}
+	void withdraw() {
+		int withdraw=500;
+		System.out.println("Ammount withdraw"+" "+(balance+withdraw));
+	}
+}
+public class AbstractMethod1 {
+	public static void main(String[] args) {
+		BankAccount obj = new CheckingAccount();
+		obj.deposit();
+		obj.withdraw();
+	}
+}
