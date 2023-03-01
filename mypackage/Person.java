6a18ca39e63a9d4fb2a981baeff128aee4d8091d
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-03-01 21:24:17 +0530

    Update Person.java

--- mypackage/Person.java
+++ mypackage/Person.java
@@ -1,4 +1,9 @@
-
+// Create a new directory on your computercalled "mypackage"
+// Inside the "mypackage" directory , create two new files: "Person.java" & "Main.java"
+// In Person.java , define a class called "Person" with two private instance variables: "name" (String) & "age" (int) 
+// & constructor that takes two arguments (name & age) & instance variables .
+// Also , than return the name and age of the person.
+// In Main.java, create a new Person object with the name "Alice" and age 25.
 public class Person { // class 1
 	// instance variable
     private String name; 
