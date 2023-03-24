4e0c4e2427331eca7f03d6ef99dbdf0324f78bf9
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-03-24 15:28:56 +0530

    Add files via upload

---
+++ CalculatorSimulator.java
@@ -0,0 +1,16 @@
+package Assignment;
+
+public class CalculatorSimulator {
+    public static void main(String[] args) {
+        TaxCalculator tc = new TaxCalculator();
+
+        try {
+            double taxAmount = tc.calculateTax("Krish", true, 600000.0);
+            System.out.println("Tax amount is " + taxAmount);
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
+
+
